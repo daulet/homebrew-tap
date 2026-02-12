9bce49cfd791ef07541214a3d12368369652afa2
Daulet Zhanguzin <daulet@zhanguzin.kz>
2026-02-12 11:12:19 -0800

    feat: release vibereview 0.1.0

--- Formula/vibereview.rb
+++ Formula/vibereview.rb
@@ -7,11 +7,11 @@ class Vibereview < Formula
   on_macos do
     on_intel do
       url "https://github.com/daulet/vibereview/releases/download/v#{version}/vibereview-x86_64-apple-darwin.tar.gz"
-      sha256 "TODO"
+      sha256 "06b07d59fdf72fb95c89ed83553c2216260b28c51201342a5ddff0b504f6007a"
     end
     on_arm do
       url "https://github.com/daulet/vibereview/releases/download/v#{version}/vibereview-aarch64-apple-darwin.tar.gz"
-      sha256 "TODO"
+      sha256 "8ffebcaf876359971685a24c27caaf258537bfec7506bb8e91381c6b2a7d1703"
     end
   end
 
